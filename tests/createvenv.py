13369143884903104d821ad7aad9ce51750409f8
hirossan4049 <haruto405329@gmail.com>
2020-06-29 16:09:47 +0900

    f commit

---
+++ tests/createvenv.py
@@ -0,0 +1,5 @@
+import subprocess
+
+py_version = "3"
+
+subprocess.run("python3 -m venv venv".split())
---
+++ tests/run.sh
@@ -0,0 +1,4 @@
+python3 testapp.py
+
+osascript -e 'tell application (path to frontmost application as text) to display dialog "Hello from osxdaily.com" buttons {"OK"} with icon stop'
+
